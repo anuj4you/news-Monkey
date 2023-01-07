9c79029a24f130da1866e7eb26c8040d33ed87cf
Anuj <anujk.581@gmail.com>
2023-01-07 03:58:47 -0500

    added Navbar and updated readMe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Weather news application!</p>
-      <h1>Hello world!</h1>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            News Monkey
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Link
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <button className="btn btn-secondary" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
